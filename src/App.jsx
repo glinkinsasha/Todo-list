810b89b0f8c0ce6378a2f3ce20f7e514568682e7
alsel <alsel1934@gmail.com>
2025-06-23 15:57:48 +0300

    add fetchTasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import './App.module.css'
+import { useState, useEffect } from 'react'
+import styles from './App.module.css'
 
 function App() {
 
   const [taskList, setTaskList] = useState([])
 
+
+
+  useEffect(() => {
+    const URL = 'http://localhost:3000/tasks';
+
+    const fetchTasks = async () => {
+      let response = await fetch(URL);
+      response.json()
+      .then( result => setTaskList(result))
+      .catch((error) => console.log(error))
+      .finally(() => {});
+    }
+    fetchTasks();
+  },[])
+
   return (
     <>
-      
+      <div>
+        {taskList.map(props => <div key={props.id} className={styles.task}>{props.message}</div>)}
+      </div>
     </>
   )
 }
