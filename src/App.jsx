96eb6d5e12471497b9f4ed186318a07f60a3b647
alsel <alsel1934@gmail.com>
2025-06-23 19:43:25 +0300

    website layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import styles from './App.module.css'
 
 function App() {
 
-  const [taskList, setTaskList] = useState([])
+  const [taskList, setTaskList] = useState([]);
+
+  const [addingTask, setAddingTask] = useState(false);
+  const [actioningTask, setActioningTask] = useState(0);
 
 
 
@@ -21,11 +24,34 @@ function App() {
   },[])
 
   return (
-    <>
-      <div>
-        {taskList.map(props => <div key={props.id} className={styles.task}>{props.message}</div>)}
+    <div className={styles.app}>
+      <input type="text" className={styles.search} placeholder='Search' />
+      <input type="button" value="Add task" className={styles.addButton} />
+      <input type="button" value="Sort tasks" className={styles.sortButton} />
+      {
+        addingTask ? 
+        <div className={styles.addTaskBlock}>
+          <input type="text" className={styles.addTaskInput} />
+          <input type="button" value="Add" className={styles.addInputButton} />
+        </div>: ''
+      }
+      
+      <div className={styles.taskList}>
+        {
+          taskList.map(props => <div key={props.id} className={styles.task}>{props.message}
+          {
+            actioningTask == props.id ?
+            <div className={styles.buttonsBlock}>
+              <button className={styles.button}>Change</button>
+              <button className={styles.button}>Delete</button>
+            </div>: ''
+          }
+            
+          </div>)
+        }
+        
       </div>
-    </>
+    </div>
   )
 }
 
